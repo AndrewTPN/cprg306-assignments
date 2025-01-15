1c7c6675d936359f5972759746df7433b9602d9b
Andrew <166637628+AndrewTPN@users.noreply.github.com>
2025-01-15 01:05:16 -0700

    Assignment week 2 is finished
    update root page.js and edit some files in week-2 folder

---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Link from 'next/link';
+
+const StudentInfo = () => {
+return (
+<div>
+    <h1>Tan Phu Nguyen - Andrew</h1>
+    <Link href="https://github.com/AndrewTPN/cprg306-assignments">GitHub Repository
+    </Link>
+</div>
+);
+};
+
+export default StudentInfo;
