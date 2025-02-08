dcd573e536ec8e0acd5fc733dff676092255ad05
Andrew <166637628+AndrewTPN@users.noreply.github.com>
2025-02-07 21:59:09 -0700

    week 5 assignment is completed
    files created, function added, tested  and ran well

---
+++ app/week-5/page.js
@@ -0,0 +1,11 @@
+"use client";
+
+import NewItem from "./new-item";
+
+export default function Page() {
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+            <NewItem />
+        </div>
+    );
+}
