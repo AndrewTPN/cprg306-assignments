b06f786a1dace213f90b2492db55393e48fea186
Andrew <166637628+AndrewTPN@users.noreply.github.com>
2025-03-08 00:11:06 -0700

    assignment 6 is complete

--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -1,11 +1,41 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Item from "./item";
 import itemsData from "./item.json";
 
 const ItemList = () => {
-    const [items] = useState(itemsData);
+
+    const [sortBy, setSortBy] = useState("name")
+    const [items, setItems] = useState(itemsData);
+
+    const sorting = () => {
+        let sortedItems = [...items];
+        if (sortBy === "name") {
+            sortedItems.sort((a,b) => a.name.localeCompare(b.name))
+
+        }
+
+        if (sortBy === "category") {
+            sortedItems.sort((a,b) => a.category.localeCompare(b.category))
+
+        }
+        setItems(sortedItems); 
+    }
+
+const handleName = () => {
+setSortBy("name")
+}
+
+const handleCategory = () => {
+setSortBy("category")
+}
+
+useEffect(() => {
+sorting()
+},[sortBy])
+
+
 
     // Render each item
     const renderItem = (item) => (
@@ -19,6 +49,9 @@ const ItemList = () => {
 
     return (
         <div className="grid grid-cols-1 gap-4 width-full md:grid-cols-2 lg:grid-cols-3">
+            <button onClick = {handleName}>Name</button>
+            <button onClick = {handleCategory}>Category</button>
+
             {items.map(renderItem)}
         </div>
     );
